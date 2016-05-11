ac8cc9a045b735a2bb4a591dd7e995f4f9b8c752
lilelr <lilelr@163.com>
2016-05-11 14:24:43 +0800

    problem 4

---
+++ Pro4.py
@@ -0,0 +1,14 @@
+__author__ = 'yuxiao'
+
+
+def compute():
+    ans = 0
+    for i in range(100,1000):
+        for j in range(100,1000):
+            k = i * j
+            s = str(k)
+            if s == s[::-1] and k > ans:
+                ans = k
+    return ans
+if __name__ == '__main__':
+    print(compute())
